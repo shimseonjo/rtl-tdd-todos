8f77c380169aaaca76f973c8f1ffc5c2afc9c9e4
shimseonjo <shimseonjo@gmail.com>
2022-03-02 20:30:57 +0900

    TotoItem refactoring ok

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React,{useCallback} from "react";
 
 const TodoItem = ({ todo, onToggle, onRemove }) => {
   const { id, text, done } = todo;
+  //onClick에서 사용하는 함수들을 useCallback을 사용하게 하면 
+  //성능을 아주 미세하게 최적화 할 수 있음.
+  const toggle = useCallback(()=>onToggle(id),[id,onToggle]);
+  const remove = useCallback(()=>onRemove(id),[id,onRemove]);
   return (
     <li>
       <span
         style={{ textDecoration: done ? "line-through" : "none" }}
-        onClick={() => {
-          onToggle(id);
-        }}
+        // onClick={() => {
+        //   onToggle(id);
+        // }}
+        onClick={toggle}
       >
         {text}
       </span>
       <button
-        onClick={() => {
-          onRemove(id);
-        }}
+        // onClick={() => {
+        //   onRemove(id);
+        // }}
+        onClick={remove}
       >
         삭제
       </button>
