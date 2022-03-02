1f0b1a8bd26896695e9a9d840eb46490bd111c75
shimseonjo <shimseonjo@gmail.com>
2022-03-02 19:10:49 +0900

    TodoItem UI test ok

---
+++ src/TodoItem.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+import { render,screen } from "@testing-library/react";
+
+describe('<TodoItem />',() => {
+    const sampleTodo = {
+        id:1,
+        text:'TDD 배우기',
+        done:false
+    };
+    const setup = (props = {}) =>{
+        const initialProps = {todo:sampleTodo};
+        const utils = render(<TodoItem {...initialProps} {...props}/>);
+        const todo = props.todo || initialProps.todo;
+        const span = screen.getByText(todo.text);
+        const button = screen.getByText('삭제');
+        return{
+            ...utils,
+            span,
+            button
+        };
+    };
+    it('has span and button',()=>{
+        const {span,button} = setup();
+        expect(span).toBeTruthy();
+        expect(button).toBeTruthy();
+    });
+});
