15f51eebc43247f89d2ebfab9e506b07a3f6e336
shimseonjo <shimseonjo@gmail.com>
2022-03-02 18:25:26 +0900

    TodoForm refactoring ok

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -5,15 +5,31 @@ import TodoForm from "./TodoForm";
 // 이 컴포넌트에는 input이 하나 있어야 하고 버튼도 하나 있어야 합니다.
 // 이에 맞춰서 테스트 코드를 작성해봅시다.
 describe("<TodoForm />", () => {
+  const setup = (props={})=>{
+    const utils = render(<TodoForm {...props}/>);
+    const input = screen.getByPlaceholderText('할 일을 입력하세요');
+    const button = screen.getByText('등록');
+    return{
+        ...utils,
+        input,
+        button
+    };
+  };
+
   it("has input and a button", () => {
-    render(<TodoForm />);
-    screen.getByText("등록");
-    screen.getByPlaceholderText("할 일을 입력하세요");
+    // render(<TodoForm />);
+    // screen.getByText("등록");
+    // screen.getByPlaceholderText("할 일을 입력하세요");
+    const{input,button} = setup();
+    expect(input).toBeTruthy();
+    expect(button).toBeTruthy();
   });
+
   //input에 change 이벤트를 발생시키면 value 값이 바뀌어야 합니다.
   it("changes input", () => {
-    render(<TodoForm />);
-    const input = screen.getByPlaceholderText("할 일을 입력하세요");
+    // render(<TodoForm />);
+    // const input = screen.getByPlaceholderText("할 일을 입력하세요");
+    const{input} = setup();
     fireEvent.change(input, {
       target: {
         value: "TDD 배우기",
@@ -29,9 +45,10 @@ describe("<TodoForm />", () => {
 
   it("calls onInsert and clears input", () => {
     const onInsert = jest.fn();
-    render(<TodoForm onInsert={onInsert} />);
-    const input = screen.getByPlaceholderText("할 일을 입력하세요");
-    const button = screen.getByText("등록");
+    // render(<TodoForm onInsert={onInsert} />);
+    // const input = screen.getByPlaceholderText("할 일을 입력하세요");
+    // const button = screen.getByText("등록");
+    const {input, button} = setup({onInsert});
     //수정하고
     fireEvent.change(input, {
       target: {
