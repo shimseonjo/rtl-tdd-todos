0d4c49d882c513dbcd0ee5b13673a8394fd4566c
shimseonjo <shimseonjo@gmail.com>
2022-03-04 14:50:43 +0900

    TotoApp onToggle event test ok

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,38 +1,50 @@
-import React,{useRef, useState, useCallback} from "react";
+import React, { useRef, useState, useCallback } from "react";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 
 const TodoApp = () => {
-    const [todos,setTodos] =useState([
-        {
-            id:1,
-            text:'TDD 배우기',
-            done:true
-        },
-        {
-            id:2,
-            text:'react-testing-library 사용하기',
-            done:true
-        }
-    ]);
-    const nextId = useRef(3);//새로 추가할 항목에서 사용할 id
-    const onInsert = useCallback(
-        text => {
-            setTodos(
-                todos.concat({
-                    id:nextId.current,
-                    text,
-                    done:false
-                })
-            );
-            //nextId값에 1더하기
-            nextId.current += 1;
-        },[todos]
-    );
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "TDD 배우기",
+      done: true,
+    },
+    {
+      id: 2,
+      text: "react-testing-library 사용하기",
+      done: true,
+    },
+  ]);
+  const nextId = useRef(3); //새로 추가할 항목에서 사용할 id
+  const onInsert = useCallback(
+    (text) => {
+      //새 항목 추가 후
+      setTodos(
+        todos.concat({
+          id: nextId.current,
+          text,
+          done: false,
+        })
+      );
+      //nextId값에 1더하기
+      nextId.current += 1;
+    },
+    [todos]
+  );
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, done: !todo.done } : todo
+        )
+      );
+    },
+    [todos]
+  );
   return (
     <>
       <TodoForm onInsert={onInsert} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onToggle={onToggle} />
     </>
   );
 };
