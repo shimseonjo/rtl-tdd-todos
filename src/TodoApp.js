8117b45d19eee3c576b10ba43f941c8fbe8a4cee
shimseonjo <shimseonjo@gmail.com>
2022-03-04 15:28:55 +0900

    TodoApp onRemove event test ok

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -41,10 +41,15 @@ const TodoApp = () => {
     },
     [todos]
   );
+  const onRemove = useCallback(
+      id => {
+          setTodos(todos.filter(todo=>todo.id !== id));
+      },[todos]
+  );
   return (
     <>
       <TodoForm onInsert={onInsert} />
-      <TodoList todos={todos} onToggle={onToggle} />
+      <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove}/>
     </>
   );
 };
